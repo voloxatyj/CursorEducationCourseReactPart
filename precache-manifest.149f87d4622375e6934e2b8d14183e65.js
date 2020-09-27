e2b7bd95b3830f2dc033ff5ef8231210f9fddbee
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-09-27 11:57:38 +0300

    Updates

---
+++ precache-manifest.149f87d4622375e6934e2b8d14183e65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5833553adc3857729d7ea2381530beed",
+    "url": "/CursorEducationCourseReactPart/index.html"
+  },
+  {
+    "revision": "3c4337b21557c8973c85",
+    "url": "/CursorEducationCourseReactPart/static/js/2.4666e5c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CursorEducationCourseReactPart/static/js/2.4666e5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4b1cea09cc0086fc74",
+    "url": "/CursorEducationCourseReactPart/static/js/main.8a6263d5.chunk.js"
+  },
+  {
+    "revision": "1f4efe0ce3b51f746f4d",
+    "url": "/CursorEducationCourseReactPart/static/js/runtime-main.e8e7de3c.js"
+  }
+]);
