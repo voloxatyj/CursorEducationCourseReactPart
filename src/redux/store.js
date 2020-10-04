84e7eeabcda9129826d2eb2a45c0901de7e76dd1
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-10-04 08:54:12 +0300

    nope

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,19 +5,39 @@ import uiReducer from './reducers/uiReducer'
 
 const initialState = {}
 
-const middleware = [thunk] || [window.thunk.default]
+// const middleware = [thunk] || [window.thunk.default]
 
-const reducers = combineReducers({
-	data: userReducer,
-	ui: uiReducer
-})
+// const reducers = combineReducers({
+// 	data: userReducer,
+// 	ui: uiReducer
+// })
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+// const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
+// const store = createStore(
+// 	reducers,
+// 	initialState,
+// 	compose( applyMiddleware(...middleware), devTools )
+// )
+
+// export default store
+
+// const createAppStore = (initialState = {}) => {
+	// const middleware = [thunk] || [window.thunk.default]
+
+	const reducers = combineReducers({
+		data: userReducer,
+		ui: uiReducer
+	})
+	const middleware = [
+		applyMiddleware(...[thunk] || [window.thunk.default]),
+		...(window.__REDUX_DEVTOOLS_EXTENSION__ ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
+	]
+// }
 const store = createStore(
-	reducers,
-	initialState,
-	compose( applyMiddleware(...middleware), devTools )
-)
+			reducers,
+			initialState,
+			compose(...middleware)
+		)
 
 export default store
